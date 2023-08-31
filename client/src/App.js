f57077414b6895268c98bbb7884af779603ef4be
Chirag Gandherva <chiraggandherva08@gmail.com>
2023-08-31 15:58:16 +0530

    updated file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const fetch_word = async (setWord) => {
+  const res = (await axios.get("http://localhost:8000/")).data;
+  console.log(res.word);
+  setWord(res.word);
+};
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const [word, setWord] = useState("");
+
+  useEffect(() => {
+    fetch_word(setWord);
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
